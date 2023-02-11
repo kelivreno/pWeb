07e6383668467ec22a59bdeed0e61232fbe96379
kelivreno <hakim.kelangit@student.greenriver.edu>
2023-02-10 16:24:09 -0800

    changed it

--- js/index.js
+++ js/index.js
@@ -11,3 +11,20 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+const form = document.querySelector('.form');
+const nameInput = document.querySelector('.form__input[type="text"]');
+const emailInput = document.querySelector('.form__input[type="email"]');
+const messageInput = document.querySelector('.form__input[placeholder="Message"]');
+const sendBtn = document.querySelector('.form__btn');
+
+sendBtn.addEventListener('click', function(event) {
+event.preventDefault();
+
+if (!nameInput.value || !emailInput.value || !messageInput.value) {
+alert("Please fill out all the required fields.");
+} else {
+alert("Thank you for your message, we will get back to you soon.");
+form.reset();
+}
+});
